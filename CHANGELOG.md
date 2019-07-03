263796ab9d7d549202314b7adb18c6d5d8f20483
Riley Bathurst <riley@rileybathurst.com>
2019-07-03 14:21:49 -0700

    v 1.1
    update from foundation theme and .md docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## Version 1.0 (November 19, 2015)
 
 Initial release.
+
+## Version 1.1 (July 3, 2019)
+
+Build from Foundation base theme to Priest Updates
--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation();
+
+// Initialize library to lazy load images
+  var observer = lozad('.lozad', {
+    threshold: 0.1,
+    load: function(el) {
+      el.src = el.getAttribute("data-src");
+    }
+  })
+
+  // Picture observer
+  // with default `load` method
+  var pictureObserver = lozad('.lozad-picture', {
+    threshold: 0.1,
+  })
+
+  // set observer
+  // with default `load` method
+  var setObserver = lozad('.lozad-set', {
+    threshold: 0.1,
+    load: function(el) {
+      el.srcset = el.getAttribute("data-srcset");
+    }
+  })
+
+  observer.observe()
+  pictureObserver.observe()
+  setObserver.observe()
